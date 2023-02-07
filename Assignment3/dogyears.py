62662a7058f4c9034d2f3802b6d4ff90714c7e3b
Prowler01 <98061132+Prowler01@users.noreply.github.com>
2023-02-07 17:02:06 -0600

    Add files via upload

---
+++ Assignment3/dogyears.py
@@ -0,0 +1,7 @@
+# This program prompts the user for the name and age of their dog. The program then calculates and displays dog's age in dog years.
+print("How old is your dog in human years? ")
+humanyears = int(input())
+print("What's your dog's name? ")
+dogname = input()
+dogyears = humanyears * 7
+print(dogname + " is " + str(dogyears) + " in dog years.")
