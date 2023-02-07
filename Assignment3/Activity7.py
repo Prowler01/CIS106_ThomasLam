8c873d045d2a34db7a561f3fb55dc147d3cc3e80
Dave Braunschweig <dave@itrain.ms>
2023-02-07 17:05:58 -0600

    Folder structure

--- Assignment3/Activity7.py
+++ Assignment3/Activity7.py
@@ -1,7 +1,8 @@
 # This program prompts the user for the name and age of their dog. The program then calculates and displays dog's age in dog years.
-print("How old is your dog in human years? ")
-humanyears = int(input())
+# References: Professor Braunsweig
 print("What's your dog's name? ")
 dogname = input()
+print("How old is your dog in human years? ")
+humanyears = int(input())
 dogyears = humanyears * 7
 print(dogname + " is " + str(dogyears) + " in dog years.")
--- Assignment3/dogyears.py
+++
@@ -1,8 +0,0 @@
-# This program prompts the user for the name and age of their dog. The program then calculates and displays dog's age in dog years.
-# References: Professor Braunsweig
-print("What's your dog's name? ")
-dogname = input()
-print("How old is your dog in human years? ")
-humanyears = int(input())
-dogyears = humanyears * 7
-print(dogname + " is " + str(dogyears) + " in dog years.")
