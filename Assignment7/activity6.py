8009aba081cbf69011ef052abd5e45e9b990a6a5
Prowler01 <98061132+Prowler01@users.noreply.github.com>
2023-03-03 01:00:16 -0600

    Add files via upload

---
+++ Assignment7/activity6.py
@@ -0,0 +1,37 @@
+# This programs determines the user's sock size
+# Based on user's shoesize input
+# References: textbook
+
+def get_shoesize():
+    print("What is your shoe size: ")
+    shoesize = int(input())
+    return shoesize
+
+
+def calculate_socksize(shoesize):
+    if shoesize < 4:
+        socksize = "Extra Small"
+        return socksize
+    elif shoesize  < 6:
+        socksize = "Small"
+        return socksize
+    elif shoesize < 9:
+        socksize = "Medium"
+        return socksize
+    elif shoesize < 12:
+        socksize = "Large"
+        return socksize
+    else:
+        socksize = "Extra Large"
+        return socksize
+
+def display_result(shoesize, socksize):
+        print("Socksize: " + str(socksize))
+        
+def main():
+       shoesize = get_shoesize()
+       socksize = calculate_socksize(shoesize)
+       display_result(shoesize, socksize)
+
+    
+main()
