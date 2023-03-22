d1dadb2b5e3f7067756d852edd33deef28e1b2af
Prowler01 <98061132+Prowler01@users.noreply.github.com>
2023-03-21 21:15:11 -0500

    Update activity1.py

--- Assignment10/activity1.py
+++ Assignment10/activity1.py
@@ -3,11 +3,13 @@
 # Then the program spits out the average test score
 # References: w3schools
 
+
 def get_score():
     print("Testscore (negative value to terminate): ")
-    score = int(input())
+    score = float(input())
    # print("Enter a negative value in to stop program.")
     return score
+
 
 def get_total(score):
     total = 0
@@ -24,15 +26,18 @@ def get_total(score):
     #output = [total, count]
     return output
 
+
 def calculate_average(output):
-    average = int(output[0] / output[1])
+    average = float(output[0] / output[1])
     print("Total: " + str(output[0]))
     print("Count: " + str(output[1]))
     return average
 
+
 def display_result(average):
     print("Average test score: " + str(average))
 
+    
 def main():
     score = get_score()
     # total = get_total(score)
