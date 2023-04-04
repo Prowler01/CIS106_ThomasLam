51ffdaac248c67c9798ae42a1290760b1a525bdc
Prowler01 <98061132+Prowler01@users.noreply.github.com>
2023-04-04 11:17:27 -0500

    Add files via upload

---
+++ Assignment10/doLoop2.py
@@ -0,0 +1,29 @@
+# This program takes user test scores and adds it to the total
+# The program repeats until the user says stop
+# Then the program spits out the average test score
+# References: w3schools
+
+def get_score():
+    print("Testscore?: ")
+    score = float(input())
+    return score
+
+def get_scores_average(score):
+    total = 0
+    count = 0
+    score = get_score()
+    do
+    {
+     
+     } while(score > 0);
+    
+def display_result(average):
+    print("Average testscore: " + str(average))
+    
+def main():
+   score = get_score()
+   average = get_scores_average(score)
+   display_result(average)
+    
+main()
+
