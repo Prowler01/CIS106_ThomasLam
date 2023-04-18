55f38e43e3dce6f35be2a1533dbd71bd860edcd4
Prowler01 <98061132+Prowler01@users.noreply.github.com>
2023-04-18 15:53:15 -0500

    Update activity1.py

--- Assignment13/activity1.py
+++ Assignment13/activity1.py
@@ -7,12 +7,12 @@
 # References: Wikiversity
 
 def get_name():
-     print("What's your fullname: ")
-     full_name = (input())
-     index = full_name.find(" ")
-     if index == 0:
-          full_name = get_name()
-     return full_name
+    print("What's your fullname: ")
+    full_name = (input())
+    index = full_name.find(" ")
+    if index == 0:
+        full_name = get_name()
+    return full_name
 
 
 def get_lastname(full_name):
