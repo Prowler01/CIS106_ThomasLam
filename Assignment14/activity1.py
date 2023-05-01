c8cbbec01c598b4a8c8c06f1ed6016f6123be2c7
Prowler01 <98061132+Prowler01@users.noreply.github.com>
2023-05-01 16:11:17 -0500

    Update activity1.py

--- Assignment14/activity1.py
+++ Assignment14/activity1.py
@@ -1,69 +1,53 @@
 # The program then calculates the highest, lowest, and average scores
-# References: askpython.com, textbook
-# Refernces: realpython.com, wikiversity
+# References: askpython.com, textbook, Techwithtim on youtube
+# References: realpython.com, wikiversity, Professor
 
 import os
 import sys
-import pandas as pd
 
-
-#def read_scores(file_path):
-#    scores = []
-#    with open(file_path, "r") as file:
-#       for line in file:
-#            score = int(line.strip())
-#            scores.append(score)
-#    return scores
-
-def create_file(filename):
-    file = open(filename, "w")
-    file.write("C\tF\n")
-    for c in range(0, 51):
-        f = c * 9 / 5 + 32
-        file.write(f"{c:.1f}\t{f:.1f}\n")
-    file.close()
-    
-def read_file(filename):
-    file = open(filename, "r")
-    for line in file:
-        line = line.strip()
-        print(line)
-    file.close()
-    print("")
-
-#def read_scores(file_path):
-#    scores = []
-#    with open(file_path, "r") as file:
-#       for line in file:
-#            score = int(line.strip())
-#            scores.append(score)
-#    return scores
-
+def read_scores(filename):
+    scores = []
+    with open(filename, 'r') as file:
+        next(file)
+        for line in file:
+            name, score = line.strip().split(',')
+            scores.append(int(score))
+        return scores
+        
 def calculate_highest(scores):
-    highest = max(scores)
+    scores.sort()
+    highest = scores[0]
     return highest
 
+
 def calculate_lowest(scores):
-    low = min(scores)
-    return low
+    scores.sort
+    lowest = scores[len(scores) - 1]
+    return lowest
 
-def calculate_average_score(scores):
-    average = sum(scores) / len(scores)
-    return sum(scores) / len(scores)
 
-def display_result(scores):
-    print("Scores:", scores)
-    print("Highest score:", highest_score)
-    print("Lowest score:", lowest_score)
-    print("Average score:", average_score)
+def calculate_average(scores):
+    sum = 0
+    for i in range(0, len(scores)):
+        sum = sum + scores[i]
+    num_elements = len(scores)
+    average = sum / num_elements
+    return average
+
+
+def display_result(highest, lowest, average):
+    print("Your lowest score is: " + str(highest))
+    print("Your highest score is: " + str(lowest))
+    print("Your average score is: " + str(average))
+
     
-
 def main():
-    score = "-scores.txt"
-    scores = read_scores("scores.txt")
-    highest_score = calculate_highest_score(scores)
-    lowest_score = calculate_lowest_score(scores)
-    average_score = calculate_average_score(scores)
-    
-    
+    filename = "scores.txt"
+    scores = read_scores(filename)
+    highest = calculate_highest(scores)
+    lowest = calculate_lowest(scores)
+    average = calculate_average(scores)
+    display_result(highest, lowest, average)
+  
+
 main()
