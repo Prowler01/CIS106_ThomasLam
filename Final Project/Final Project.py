de58527d0df51b66fa0eb4c506d103d34219b968
Prowler01 <98061132+Prowler01@users.noreply.github.com>
2023-05-13 14:54:33 -0500

    Final Project.py

--- Final Project/Final Project.py
+++ Final Project/Final Project.py
@@ -1,26 +1,31 @@
 import xml.etree.ElementTree as ET
 
-tree = ET.parse('cd_catalog.xml')
-root = tree.getroot()
+def parse_xml(xml_file):
+    tree = ET.parse(xml_file)
+    root = tree.getroot()
 
+    titles = []
+    artists = []
+    countries = []
+    prices = []
+    years = []
 
-titles = []
-artists = []
-countries = []
-prices = []
-years = []
+    for cd in root.findall('CD'):
+        titles.append(cd.find('TITLE').text)
+        artists.append(cd.find('ARTIST').text)
+        countries.append(cd.find('COUNTRY').text)
+        prices.append(cd.find('PRICE').text)
+        years.append(cd.find('YEAR').text)
 
+    return titles, artists, countries, prices, years
 
-for cd in root.findall('CD'):
-    titles.append(cd.find('TITLE').text)
-    artists.append(cd.find('ARTIST').text)
-    countries.append(cd.find('COUNTRY').text)
-    prices.append(cd.find('PRICE').text)
-    years.append(cd.find('YEAR').text)
+def print_catalog(titles, artists, countries, prices, years):
+    count =  0
+    while count < len(titles):
+        print([titles[count], artists[count], countries[count], prices[count], years[count]])
+        count += 1
 
-
-count =  0
-while count <= 25:
-    print([titles[count], artists[count], countries[count], prices[count], years[count]])
-    count =  count +1
-
+if __name__ == "__main__":
+    xml_file = 'cd_catalog.xml'
+    titles, artists, countries, prices, years = parse_xml(xml_file)
+    print_catalog(titles, artists, countries, prices, years)
