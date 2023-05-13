51a6d65f2c8e5595b17611da8eacd586964406ca
Prowler01 <98061132+Prowler01@users.noreply.github.com>
2023-05-13 15:50:51 -0500

    Final Project.py

--- Final Project/Final Project.py
+++ Final Project/Final Project.py
@@ -5,57 +5,57 @@
 # displayed
 # References: datacamp.com, stackoverflow.com
 
-import xml.etree.ElementTree as ET
-
-def get_cd_data(filename):
-    try:
-        tree = ET.parse(filename)
-    except:
-        print("Error: Missing or bad data")
-        return None
-    
-    root = tree.getroot()
-
+def parse_xml_file(xml_file):
+    with open(xml_file, 'r') as file:
+        xml_data = file.read()
+    start_tag = '<CD>'
+    end_tag = '</CD>'
+    cd_list = xml_data.split(start_tag)[1:]
     titles = []
     artists = []
     countries = []
     prices = []
     years = []
+    for cd in cd_list:
+        title_start = cd.find('<TITLE>') + 7
+        title_end = cd.find('</TITLE>')
+        titles.append(cd[title_start:title_end])
+        artist_start = cd.find('<ARTIST>') + 8
+        artist_end = cd.find('</ARTIST>')
+        artists.append(cd[artist_start:artist_end])
+        country_start = cd.find('<COUNTRY>') + 9
+        country_end = cd.find('</COUNTRY>')
+        countries.append(cd[country_start:country_end])
+        price_start = cd.find('<PRICE>') + 7
+        price_end = cd.find('</PRICE>')
+        prices.append(cd[price_start:price_end])
+        year_start = cd.find('<YEAR>') + 6
+        year_end = cd.find('</YEAR>')
+        years.append(cd[year_start:year_end])
+    return titles, artists, countries, prices, years
 
-    for cd in root.findall('CD'):
-        titles.append(cd.find('TITLE').text)
-        artists.append(cd.find('ARTIST').text)
-        countries.append(cd.find('COUNTRY').text)
-        prices.append(cd.find('PRICE').text)
-        years.append(cd.find('YEAR').text)
 
-    cd_data = []
-
+def display_result(xml_file):
+    titles, artists, countries, prices, years = parse_xml_file(xml_file)
+    if len(titles) == 0:
+        print("Error: Missing or bad data")
+        return
     for i in range(len(titles)):
-        cd_info = titles[i] + " - " + artists[i] + " - " + countries[i] + " - " + prices[i] + " - " + years[i]
-        cd_data.append(cd_info)
+        print(f"{titles[i]} - {artists[i]} - {countries[i]} - {prices[i]} - {years[i]}")
 
-    return cd_data
 
-def get_cd_stats(cd_data):
-    num_cds = len(cd_data)
-    total_price = 0
+def display_result2(xml_file):
+    titles, artists, countries, prices, years = parse_xml_file(xml_file)
+    if len(titles) == 0:
+        print("Error: Missing or bad data")
+        return
+    num_items = len(titles)
+    avg_price = sum([float(price) for price in prices]) / num_items
+    print(f"Number of items: {num_items}")
+    print(f"Average price: {avg_price:.2f}")
 
-    for cd in cd_data:
-        price = float(cd.split(" - ")[3])
-        total_price += price
 
-    avg_price = total_price / num_cds
-
-    return [num_cds, avg_price]
-
-filename = 'cd_catalog.xml'
-cd_data = get_cd_data(filename)
-
-if cd_data:
-    for cd_info in cd_data:
-        print(cd_info)
-
-    cd_stats = get_cd_stats(cd_data)
-    print("\nTotal CDs: " + str(cd_stats[0]))
-    print("Average price: " + "{:.2f}".format(cd_stats[1]))
+if __name__ == "__main__":
+    xml_file = "cd_catalog.xml"
+    display_result(xml_file)
+    display_result2(xml_file)
