52cf21c64fa38ecf57ceb3eba2c94c19f4d4f627
Prowler01 <98061132+Prowler01@users.noreply.github.com>
2023-05-13 14:58:01 -0500

    Final Project.py

--- Final Project/Final Project.py
+++ Final Project/Final Project.py
@@ -1,7 +1,7 @@
 import xml.etree.ElementTree as ET
 
-def parse_xml(xml_file):
-    tree = ET.parse(xml_file)
+def parse_xml(filename):
+    tree = ET.parse(filename)
     root = tree.getroot()
 
     titles = []
@@ -14,18 +14,24 @@ def parse_xml(xml_file):
         titles.append(cd.find('TITLE').text)
         artists.append(cd.find('ARTIST').text)
         countries.append(cd.find('COUNTRY').text)
-        prices.append(cd.find('PRICE').text)
-        years.append(cd.find('YEAR').text)
+        prices.append(float(cd.find('PRICE').text))
+        years.append(int(cd.find('YEAR').text))
 
     return titles, artists, countries, prices, years
 
-def print_catalog(titles, artists, countries, prices, years):
-    count =  0
-    while count < len(titles):
-        print([titles[count], artists[count], countries[count], prices[count], years[count]])
-        count += 1
+def display_cd_catalog(filename):
+    titles, artists, countries, prices, years = parse_xml(filename)
 
-if __name__ == "__main__":
-    xml_file = 'cd_catalog.xml'
-    titles, artists, countries, prices, years = parse_xml(xml_file)
-    print_catalog(titles, artists, countries, prices, years)
+    for i in range(len(titles)):
+        print([titles[i], artists[i], countries[i], prices[i], years[i]])
+
+def cd_catalog_stats(filename):
+    _, _, _, prices, _ = parse_xml(filename)
+    num_items = len(prices)
+    avg_price = sum(prices) / num_items
+
+    return [num_items, avg_price]
+
+stats = cd_catalog_stats('cd_catalog.xml')
+print('Number of items:', stats[0])
+print('Average price:', stats[1])
